701752da7eb66b695b91a016a1b2b9fc6f33e694
Angelos Orfanakos <git@angelos.dev>
2025-08-09 09:22:53 +0300

    Print usage information on missing cli arguments

--- fit2gpx/cli.py
+++ fit2gpx/cli.py
@@ -6,6 +6,10 @@ from time import sleep
 from .fit_converter import FitConverter
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: fit2gpx <file1.fit> [file2.fit ...]")
+        sys.exit(1)
+
     for fit_path in sys.argv[1:]:
         FitConverter(fit_path).convert()
         sleep(1) # Max 1 req/sec
